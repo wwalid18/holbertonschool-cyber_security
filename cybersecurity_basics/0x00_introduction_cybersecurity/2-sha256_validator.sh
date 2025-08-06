75101bd6368bc70ce494cfda6debd49951756440
wwalid18 <agar.io.annonyms@gmail.com>
2025-08-06 13:25:14 +0100

    task 2

--- cybersecurity_basics/0x00_introduction_cybersecurity/2-sha256_validator.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/2-sha256_validator.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sha256sum "$1" | awk '{print $1}' | grep -q "^$2$" && echo "$1: OK" || echo "$1: FAILED"
+[ "$(sha256sum "$1" | cut -d' ' -f1 | grep -c "^$2$")" -eq 1 ] && echo "$1: OK" || echo "$1: FAILED"
