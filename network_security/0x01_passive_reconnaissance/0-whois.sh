5b048da3c50b414b56b66b8817fdbd20e94f092f
wwalid18 <agar.io.annonyms@gmail.com>
2025-08-17 18:44:06 +0100

    task 0

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois $1 | awk -F': ' '/Registrant|Admin|Tech/ {gsub(/^ +| +$/, "", $2); print $1","$2}' > "$1.csv"
+whois $1 | awk -F': ' '/^(Registrant|Admin|Tech) (Name|Organization|Street|City|State\/Province|Postal Code|Country|Phone|Phone Ext:|Fax|Fax Ext:|Email)/ {gsub(/^ +| +$/, "", $2); print $1" "$2","$2}' > "$1.csv"
