15dc94a8c4e9984da43d64870da19fb9d44f3575
wwalid18 <agar.io.annonyms@gmail.com>
2025-11-04 17:06:08 +0100

    task9

--- linux_security/0x03_protocols_servers/9-firewall.sh
+++ linux_security/0x03_protocols_servers/9-firewall.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-sudo iptables -F && iptables -A INPUT -p tcp --dport 22 -j ACCEPT && iptables -A INPUT -j DROP
+sudo iptables -F; sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+sudo iptables -A INPUT -j DROP
