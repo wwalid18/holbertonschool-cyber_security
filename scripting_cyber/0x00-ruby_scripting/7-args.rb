40b05b4146ede7402ddfdb679c85efc84e41927c
wwalid18 <agar.io.annonyms@gmail.com>
2026-02-16 17:59:33 +0100

    task 7

--- scripting_cyber/0x00-ruby_scripting/7-args.rb
+++ scripting_cyber/0x00-ruby_scripting/7-args.rb
@@ -5,6 +5,12 @@ def print_arguments
   if ARGV.empty?
     puts 'No arguments provided.'
   else
+    # Check if all arguments are numbers
+    all_numbers = ARGV.all? { |arg| arg.match?(/^\d+$/) }
+    
+    puts 'Arguments:' if all_numbers
+    puts if all_numbers  # Add blank line after Arguments:
+    
     ARGV.each_with_index do |arg, index|
       puts "#{index + 1}. #{arg}"
     end
