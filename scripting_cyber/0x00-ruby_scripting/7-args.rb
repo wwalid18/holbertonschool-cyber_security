8ce8e243a52ba9ce164d054233e1c90089372121
wwalid18 <agar.io.annonyms@gmail.com>
2026-02-16 17:49:03 +0100

    task 7

--- scripting_cyber/0x00-ruby_scripting/7-args.rb
+++ scripting_cyber/0x00-ruby_scripting/7-args.rb
@@ -6,8 +6,9 @@ def print_arguments
     puts 'No arguments provided.'
   else
     puts 'Arguments:'
-    ARGV.each do |arg|
-      puts arg
+    puts
+    ARGV.each_with_index do |arg, index|
+      puts "#{index + 1}. #{arg}"
     end
   end
 end
