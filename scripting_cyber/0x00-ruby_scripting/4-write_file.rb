319d79cb476ee0e7989a942b00bb5f5190be206e
wwalid18 <agar.io.annonyms@gmail.com>
2026-02-16 17:34:14 +0100

    task 4

---
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+require 'json'
+
+def merge_json_files(file1_path, file2_path)
+  return unless File.exist?(file1_path) && File.exist?(file2_path)
+
+  file1_content = File.read(file1_path)
+  file2_content = File.read(file2_path)
+
+  data1 = JSON.parse(file1_content)
+  data2 = JSON.parse(file2_content)
+
+  merged_data = data2 + data1
+
+  File.open(file2_path, 'w') do |f|
+    f.write(JSON.pretty_generate(merged_data))
+  end
+end
