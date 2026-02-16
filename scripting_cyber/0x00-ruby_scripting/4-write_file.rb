7739019f665fcd9123727ea1dda7316115efa96f
wwalid18 <agar.io.annonyms@gmail.com>
2026-02-16 17:37:23 +0100

    task 4

--- scripting_cyber/0x00-ruby_scripting/4-write_file.rb
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -15,4 +15,6 @@ def merge_json_files(file1_path, file2_path)
   File.open(file2_path, 'w') do |f|
     f.write(JSON.pretty_generate(merged_data))
   end
+
+  puts "Merged JSON written to #{file2_path}"
 end
