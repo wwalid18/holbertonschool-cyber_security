9deddee26a93da4adb8396794ecfb9ff71f4a348
wwalid18 <agar.io.annonyms@gmail.com>
2026-02-16 18:02:14 +0100

    task 8

---
+++ scripting_cyber/0x00-ruby_scripting/8-post.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+require 'net/http'
+require 'uri'
+require 'json'
+
+def post_request(url, body_params = {})
+  uri = URI(url)
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = uri.scheme == 'https'
+
+  request = Net::HTTP::Post.new(uri.path, { 'Content-Type' => 'application/json' })
+  request.body = body_params.to_json unless body_params.empty?
+
+  response = http.request(request)
+
+  puts "Response status: #{response.code} #{response.message}"
+  puts "Response body:"
+
+  begin
+    json_body = JSON.parse(response.body)
+    puts JSON.pretty_generate(json_body)
+  rescue JSON::ParserError
+    puts response.body
+  end
+end
